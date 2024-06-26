69bf2335513d8a35e7e915e3533ac33555f61ab5
PiyushMhetre <118716204+PiyushMhetre@users.noreply.github.com>
2024-06-26 19:07:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("hit index.js in frontend ")
 root.render(
   <React.StrictMode>
     <App />
